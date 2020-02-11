98c6aed9b56208e1d0c1ab0cf52a4e0f150c5097
Yuanqing Mei <njumyq@outlook.com>
2020-02-11 11:15:19 +0800

    python style

---
+++ pythonStyle.py
@@ -0,0 +1,20 @@
+# _*_ coding: utf-8 _*_
+'''
+__author__ = 'Yuanqing Mei'
+__email__ = 'dg1533019@smail.nju.edu.cn'
+__file__ = pythonStyle
+__time__ = 2020/2/11 11:10
+__description__ = ''
+'''
+
+import sys
+
+def main(argv):
+
+    '''Do something...'''
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main(sys.argv[1:]))
